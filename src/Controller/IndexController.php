075de54c4b4f75b4ddbd6586743f10b468cac9f2
Matteotr9 <89576532+Matteotr9@users.noreply.github.com>
2022-11-21 16:36:59 +0100

    réparation du problème de repository ou il manqué un s

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -7,7 +7,6 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\CoursRepository;
 
 class IndexController extends AbstractController
 {
@@ -20,7 +19,7 @@ class IndexController extends AbstractController
         ]);
     }
 
-    #[Route('/cours', name: 'app_cours')]
+    #[Route('/index/cours', name: 'app_cours')]
     public function coursAction(ManagerRegistry $doctrine): Response
     {
         $cours=$doctrine ->getRepository (Cours :: class)->findAll();
