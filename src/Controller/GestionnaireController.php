f64e863058eb8e32a9f017cf7950c87d6cd738e1
Matteotr9 <89576532+Matteotr9@users.noreply.github.com>
2022-11-29 10:58:58 +0100

    création responsable manuelle

--- src/Controller/GestionnaireController.php
+++ src/Controller/GestionnaireController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Responsable;
+use App\Form\CreateResponsableType;
 
 class GestionnaireController extends AbstractController
 {
@@ -14,5 +16,22 @@ class GestionnaireController extends AbstractController
         return $this->render('gestionnaire/index.html.twig', [
             'controller_name' => 'GestionnaireController',
         ]);
+    }
+
+    public function addResponsable(Request $request, ManagerRegistry $doctrine)
+    {
+
+        $responsable = new Responsable();
+        $form = $this->createForm(CreateResponsableType::class, $responsable);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $responsable = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($responsable);
+            $entityManager->flush();
+            return $this->render('responsable/form.html.twig', ['responsable' => $responsable,]);
+        } else {
+            return $this->render('responsable/form.html.twig', array('form' => $form->createView(),));
+        }
     }
 }
