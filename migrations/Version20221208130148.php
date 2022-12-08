fee8258533898493acbf1aa2a00af7ee7ac19318
SKORPIA <99028310+SK0RPIA@users.noreply.github.com>
2022-12-08 14:30:20 +0100

    feat :recycle: nettoyage migrations

--- migrations/Version20221208130148.php
+++ migrations/Version20221208130148.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221117143006 extends AbstractMigration
+final class Version20221208130148 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,6 +22,7 @@ final class Version20221117143006 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE enfant (id INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE enfant ADD CONSTRAINT FK_34B70CA2BF396750 FOREIGN KEY (id) REFERENCES user (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE responsable ADD CONSTRAINT FK_52520D07BF396750 FOREIGN KEY (id) REFERENCES user (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user DROP extend_id');
     }
 
@@ -30,6 +31,7 @@ final class Version20221117143006 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE enfant DROP FOREIGN KEY FK_34B70CA2BF396750');
         $this->addSql('DROP TABLE enfant');
+        $this->addSql('ALTER TABLE responsable DROP FOREIGN KEY FK_52520D07BF396750');
         $this->addSql('ALTER TABLE user ADD extend_id INT NOT NULL');
     }
 }
