9809b19e817e35e823402e08dfc0f217bf9aee03
SKORPIA <99028310+SK0RPIA@users.noreply.github.com>
2022-12-09 09:12:45 +0100

    feat :sparkles: Modifications templates enfant

--- src/Entity/Responsable.php
+++ src/Entity/Responsable.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ResponsableRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ResponsableRepository::class)]
@@ -13,9 +15,47 @@ class Responsable extends User
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\OneToMany(mappedBy: 'responsable', targetEntity: Enfant::class, orphanRemoval: true)]
+    private Collection $enfants;
+
+    public function __construct()
+    {
+        $this->enfants = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return Collection<int, Enfant>
+     */
+    public function getEnfants(): Collection
+    {
+        return $this->enfants;
+    }
+
+    public function addEnfant(Enfant $enfant): self
+    {
+        if (!$this->enfants->contains($enfant)) {
+            $this->enfants->add($enfant);
+            $enfant->setResponsable($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEnfant(Enfant $enfant): self
+    {
+        if ($this->enfants->removeElement($enfant)) {
+            // set the owning side to null (unless already changed)
+            if ($enfant->getResponsable() === $this) {
+                $enfant->setResponsable(null);
+            }
+        }
+
+        return $this;
     }
 
 }
